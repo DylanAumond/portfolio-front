c09839c741fb8622c5fdd1420f78c73f3905771e
Dylan Aumond <dylanaumond@gmail.com>
2022-03-17 16:41:08 +0100

    ajout slider customer

--- src/components/CustomerSlider.js
+++ src/components/CustomerSlider.js
@@ -1,21 +1,33 @@
 import React from "react";
+import Slider from "react-slick";
 
 export default function CustomerSlider({ customers }) {
+  const settings = {
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    lazyload: true,
+    autoplay: true,
+    autoplaySpeed: 0,
+  };
   return (
-    <div className="flex justify-around">
-      {customers.map((customer) => {
-        return (
-          <div
-            className="overflow-hidden w-32 h-32 bg-cover"
-            key={customer._id}
-            style={{
-              backgroundImage: `url(http://localhost:5000/public/images/${customer.logo})`,
-            }}
-          >
-            <p>{customer.libelle}</p>
-          </div>
-        );
-      })}
+    <div>
+      <Slider {...settings}>
+        {customers.map((customer, i) => {
+          return (
+            <div className="w-32 h-32" data-index={i} key={i}>
+              <div
+                className="w-1/4 h-5/6 m-auto"
+                style={{
+                  backgroundImage: `url(http://localhost:5000/public/images/${customer.logo})`,
+                }}
+              ></div>
+              <p>{customer.libelle}</p>
+            </div>
+          );
+        })}
+      </Slider>
     </div>
   );
 }
