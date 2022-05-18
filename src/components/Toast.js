7d7b12a90fe82bdc1f599202b2049498a7daab10
Dylan Aumond <dylanaumond@gmail.com>
2022-05-18 20:59:35 +0200

    css toast

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,11 +1,35 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { DELETE_TOAST } from "../constant/toasts";
+import {
+  AiFillCheckCircle,
+  AiFillExclamationCircle,
+  AiFillInfoCircle,
+  AiFillWarning,
+} from "react-icons/ai";
 
-export default function Toast({ text, color, id }) {
+export default function Toast({ text, color, id, icon }) {
   const dispatch = useDispatch();
+  const displayIcon = (icon) => {
+    switch (icon) {
+      case "checked":
+        return <AiFillCheckCircle className="w-8 h-8" />;
+      case "error":
+        return <AiFillExclamationCircle className="w-8 h-8" />;
+      case "info":
+        return <AiFillInfoCircle className="w-8 h-8" />;
+      case "warning":
+        return <AiFillWarning className="w-8 h-8" />;
+      default:
+        return <AiFillWarning className="w-8 h-8" />;
+        break;
+    }
+  };
   return (
-    <div className={`bg-${color} text-white flex justify-between`}>
+    <div
+      className={`bg-${color} rounded-lg my-4 mr-2 text-white flex justify-around items-center h-12`}
+    >
+      {displayIcon(icon)}
       <p>{text}</p>
       <button onClick={() => dispatch({ type: DELETE_TOAST, payload: id })}>
         X
