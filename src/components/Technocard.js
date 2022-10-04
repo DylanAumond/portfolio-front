7737608b2e078a6e1666d6068ac52acbe5641f14
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-10-04 15:43:56 +0200

    technocard style

--- src/components/Technocard.js
+++ src/components/Technocard.js
@@ -3,12 +3,10 @@ import React from "react";
 export default function Technocard({ technologie }) {
   return (
     <div
-      className="octan"
+      className="octan mt-14 m-auto"
       key={technologie._id}
       style={{
-        ":after": {
-          backgroundImage: `url(${process.env.REACT_APP_API_URL}/public/images/${technologie.logo})`,
-        },
+        backgroundImage: `url(${process.env.REACT_APP_API_URL}/public/images/${technologie.logo})`,
       }}
     ></div>
   );
