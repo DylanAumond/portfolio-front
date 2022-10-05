48329d8505782c3ea291548d3b630114abe2caae
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-10-05 16:09:45 +0200

    fix

--- src/components/Technocard.js
+++ src/components/Technocard.js
@@ -1,47 +1,41 @@
 import React from "react";
-import glamorous from "glamorous"
+import glamorous from "glamorous";
 
-export default function Technocard({ technologie,size }) {
-  const Hexa = glamorous.div(
-  {
-    marginTop: `${size/4+5}px`,
-    marginRight: '5px',
+export default function Technocard({ technologie, size }) {
+  const Hexa = glamorous.div({
+    marginTop: `${size / 4 + 5}px`,
+    marginRight: "5px",
     width: `${size}px`,
-    height: `${size/2}px`,
-    position: 'relative',
-    background: '#32557f',
+    height: `${size / 2}px`,
+    position: "relative",
+    background: "#FF1616",
     backgroundImage: `url(${process.env.REACT_APP_API_URL}/public/images/${technologie.logo})`,
-    backgroundSize: 'contain',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    ":before":{
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    ":before": {
       content: `''`,
-      position: 'absolute',
-      top: '-50%',
+      position: "absolute",
+      top: "-50%",
       left: 0,
       width: 0,
       height: 0,
-      borderLeft: `${size/2}px solid transparent`,
-      borderRight: `${size/2}px solid transparent`,
-      borderBottom: `${size/4}px solid #32557f`,
+      borderLeft: `${size / 2}px solid transparent`,
+      borderRight: `${size / 2}px solid transparent`,
+      borderBottom: `${size / 4}px solid #FF1616`,
     },
-    ":after":{
+    ":after": {
       content: `''`,
-      position: 'absolute',
-      bottom: '-50%',
+      position: "absolute",
+      bottom: "-50%",
       left: 0,
       width: 0,
       height: 0,
-      borderLeft: `${size/2}px solid transparent`,
-      borderRight: `${size/2}px solid transparent`,
-      borderTop: `${size/4}px solid #32557f`,
-    }
-  },
-)
+      borderLeft: `${size / 2}px solid transparent`,
+      borderRight: `${size / 2}px solid transparent`,
+      borderTop: `${size / 4}px solid #FF1616`,
+    },
+  });
 
-  return (
-    <Hexa
-      key={technologie._id}
-    />
-  );
+  return <Hexa key={technologie._id} />;
 }
