2fcb5485824bfe9671abc5b223eab8898365f2ef
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-12-09 14:58:36 +0100

    style page 403 404

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function Page404() {
   return (
-    <>
-      <p className="text-9xl">404</p>
-      <p className="text-xl">La page n'existe pas</p>
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div>
+        <p className="text-9xl">404</p>
+        <p className="text-xl">La page n'existe pas</p>
+      </div>
+    </div>
   );
 }
