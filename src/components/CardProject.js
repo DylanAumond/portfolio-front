2b2d2a148545abd7b34cefa063cf8ed7dd36ea1c
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-12-26 22:43:35 +0100

    test scroll animation

--- src/components/CardProject.js
+++ src/components/CardProject.js
@@ -3,22 +3,21 @@ import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { PROJECT } from "../constant/Modal";
 
-export default function CardProject({ project, index}) {
+export default function CardProject({ project, index }) {
   // get the translations from home
   const { t } = useTranslation("Home");
   const dispatch = useDispatch();
-  const [hover,setHover] = useState(false);
+  const [hover, setHover] = useState(false);
   return (
-    <div className={`mt-${(index%3)*8}`}>
+    <div className={`mt-${(index % 3) * 8}`}>
       <div
-        onMouseOver={()=>setHover(true)}
-        onMouseLeave={()=>setHover(false)}
-        className='relative w-full bg-cover bg-center cursor-pointer h-96 sm:h-60 lg:h-96'
+        onMouseOver={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        className="relative w-full bg-cover bg-center cursor-pointer h-96 sm:h-60 lg:h-96"
         style={{
           backgroundImage: `url(${process.env.REACT_APP_API_URL}/public/images/${project.imgs[0]})`,
         }}
       >
-
         <div className="absolute -top-4 -left-4">
           <div className="w-6 h-2 bg-red"></div>
           <div className="w-2 h-4 bg-red"></div>
@@ -30,12 +29,17 @@ export default function CardProject({ project, index}) {
         </div>
 
         <div
-        className="h-full w-full flex justify-center items-center"
-        onClick={()=>dispatch({type: PROJECT, project})}
-        style={ hover === true ? {backgroundColor:'rgba(255, 255, 255, 0.6)'} : {display: 'none'}}>
-          <div style={{color:'black'}}>
+          className="h-full w-full flex justify-center items-center"
+          onClick={() => dispatch({ type: PROJECT, project })}
+          style={
+            hover === true
+              ? { backgroundColor: "rgba(255, 255, 255, 0.6)" }
+              : { display: "none" }
+          }
+        >
+          <div style={{ color: "black" }}>
             <p>{project.libelle}</p>
-            <p>{t('SeeProject')}</p>
+            <p>{t("SeeProject")}</p>
           </div>
         </div>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,12 @@ import { getTechnologies } from "../api/technologies";
 
 import Loader from "../components/Loader";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
 const CustomerSlider = React.lazy(() => import("../components/CustomerSlider"));
 const CardProject = React.lazy(() => import("../components/CardProject"));
 const Technocard = React.lazy(() => import("../components/Technocard"));
@@ -27,11 +33,57 @@ export default function Home() {
   // get the technologies from the technologies' reducer
   const { technologies } = useSelector(state => state.technologiesReducer);
 
+  // animation technologies
+  const slideInTop = (elem, delay, duration) => {
+    gsap.fromTo(
+      elem,
+      {
+        opacity: 0,
+        y: -200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: delay || 0.4,
+        duration: duration || 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "bottom center",
+        },
+      }
+    );
+  };
+
+  const slideInLeft = (elem, delay, duration) => {
+    gsap.fromTo(
+      elem,
+      {
+        opacity: 0,
+        x: -200,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        delay: delay || 0.4,
+        duration: duration || 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "bottom center",
+        },
+      }
+    );
+  };
+
   // rehydrate the reducers on dispatch action
   useEffect(() => {
     dispatch(getCustomers());
     dispatch(getProjects());
     dispatch(getTechnologies());
+    slideInTop("#customers", "1", "1");
+    slideInLeft("#project", "1", "1");
+    slideInLeft("#cardTechno", "1", "1");
   }, [dispatch]);
 
   return (
@@ -53,7 +105,7 @@ export default function Home() {
       </div>
 
       {/* cutomers' Section */}
-      <div className="text-center p-4 sm:mt-0">
+      <div id="customers" className="text-center p-4 sm:mt-0">
         {/* customer's section title */}
         <div className="w-24 h-auto m-auto">
           <h2 className=" text-2xl">{t("Customers")}</h2>
@@ -67,7 +119,7 @@ export default function Home() {
       </div>
 
       {/* Projects' Section */}
-      <div className="text-black  sm:mx-8 mt-16">
+      <div id="project" className="text-black lg:mt-24 sm:mx-8 mt-16">
         {/* Project's section tilte */}
         <div className="w-24 h-auto m-auto">
           <h2 className=" text-2xl">{t("Projects")}</h2>
@@ -84,14 +136,17 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-white mb-10 mt-16 flex flex-col justify-center items-center">
+      <div className="bg-white mb-10 lg:mt-24 flex flex-col justify-center items-center">
         <div className="w-40">
           <h2 className=" text-2xl">{t("Technology")}</h2>
           <div className="h-1 bg-red w-8 mb-5 "></div>
         </div>
 
         {/* Technologies' list */}
-        <div className="grid md:grid-cols-4  grid-cols-2 gap-2 md:gap-4 ">
+        <div
+          id="cardTechno"
+          className="grid md:grid-cols-4  grid-cols-2 gap-2 md:gap-4 "
+        >
           {technologies.map((technology, index) => (
             <Suspense key={index} fallback={<Loader />}>
               <Technocard key={index} technology={technology} />
