9249c677460babe63999e0e69b1262e3023780e4
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-12-26 20:12:01 +0100

    add toast to panel admin technologies

--- src/components/admin/FormRole.js
+++ src/components/admin/FormRole.js
@@ -11,33 +11,34 @@ export default function FormRole() {
 
   const [role, setRole] = useState(initRole);
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setRole({ ...role, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     dispatch(createRole(role));
   };
 
   return (
-    <div>
-      <h1>Add a role</h1>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        
-        <input
-          type={"text"}
-          placeholder="role libelle"
-          name="libelle"
-          value={role.libelle}
-          onChange={(e) => handleChange(e)}
-        />
+    <div className="ml-8 mt-8 w-5/6 flex justify-center">
+      <div>
+        <h1>Add a role</h1>
+        <form onSubmit={e => handleSubmit(e)}>
+          <input
+            className="w-72 border-y-2 my-6"
+            type={"text"}
+            placeholder="role libelle"
+            name="libelle"
+            value={role.libelle}
+            onChange={e => handleChange(e)}
+          />
 
-        <button type="submit"  className=" bg-green-400 w-48 mx-8 rounded-lg">
-          create
-        </button>
-
-      </form>
+          <button type="submit" className=" bg-green-400 w-48 mx-8 rounded-lg">
+            create
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
