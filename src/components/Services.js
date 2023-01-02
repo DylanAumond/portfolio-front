06ae07e0b34ee0a0fd104629f60b0662ae48ab53
DylanAumond <dylanaumond@gmail.com>
2023-01-02 15:15:47 +0100

    service

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,19 +7,19 @@ export default function Services() {
     <div className='grid grid-cols-3 gap-8'>
       <div className='bg-whiteCard  flex items-center rounded-full'>
         <Lottie className='w-32 h-32 rounded-full bg-whiteCard border-4 border-red' animationData={responsive} loop={true} />
-        <p className='w-3/4'>Développez <span className='text-red'>votre audience</span> sur toutes les plateformes grâce à un <span className='text-red'>design responsive .</span></p>
+        <p className='w-3/4'>Développez <span className='text-red'>votre audience</span> sur toutes les plateformes grâce à un <span className='text-red'>design responsive</span> .</p>
       </div>
       <div className='bg-whiteCard  flex items-center rounded-full'>
         <Lottie className='w-32 h-32 rounded-full bg-whiteCard border-4 border-red' animationData={responsive} loop={true} />
-        <p className='w-3/4'>Augmenter <span className='text-red'>votre visibilité</span> sur le web et gagner de nouveaux clients grâce au <span className='text-red'>référencement .</span></p>
+        <p className='w-3/4'>Augmenter <span className='text-red'>votre visibilité</span> sur le web et gagner de nouveaux clients grâce au <span className='text-red'>référencement</span> .</p>
       </div>
       <div className='bg-whiteCard  flex items-center rounded-full'>
         <Lottie className='w-32 h-32 rounded-full bg-whiteCard border-4 border-red' animationData={responsive} loop={true} />
-        <p className='w-3/4'>Vendez <span className='text-red'>vos produits</span> en ligne dans le monde entier grâce à un <span className='text-red'>site vitrine</span></p>
+        <p className='w-3/4'>Vendez <span className='text-red'>vos produits</span> en ligne dans le monde entier grâce à un <span className='text-red'>site marchand</span> .</p>
       </div>
       <div className='bg-whiteCard  flex items-center rounded-full'>
         <Lottie className='w-32 h-32 rounded-full bg-whiteCard border-4 border-red' animationData={responsive} loop={true} />
-        <p className='w-3/4'>Développez votre audience sur toutes les plateformes grâce à un <span className='text-red'>design responsive</span></p>
+        <p className='w-3/4'>Représentez <span className='text-red'>votre activité</span> en ligne grâce à un <span className='text-red'>site vitrine</span></p>
       </div>
       <div className='bg-whiteCard  flex items-center rounded-full'>
         <Lottie className='w-32 h-32 rounded-full bg-whiteCard border-4 border-red' animationData={responsive} loop={true} />
